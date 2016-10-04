fb031458798efced66c7cf4ebdbf35591d578b9c
mvasanth <mvasanth@mvasanth-VirtualBox>
2016-10-03 21:58:40 -0400

    Enhancement to exercise 7.5: removed unnecessary loop.

--- exercise7.6/merge.c
+++ exercise7.6/merge.c
@@ -13,37 +13,34 @@ void merge_sorted_arrays(int arr1[], int size1, int arr2[], int size2, int merge
     int i = 0;
     int j = 0;
     
-    while(k < size_merged)
+    while((i < size1) && (j < size2))
     {
-        while((i < size1) && (j < size2))
-        {
-            if(arr1[i] <= arr2[j])
-            {
-                merged_arr[k] = arr1[i];
-                ++i;
-            }
-            else
-            {
-                merged_arr[k] = arr2[j];
-                ++j;
-            }
-
-            ++k;
-        }
-        
-        while(i < size1)
+        if(arr1[i] <= arr2[j])
         {
             merged_arr[k] = arr1[i];
             ++i;
-            ++k;
         }
-        
-        while(j < size2)
+        else
         {
             merged_arr[k] = arr2[j];
             ++j;
-            ++k;
         }
+
+        ++k;
+    }
+           
+    while(i < size1)
+    {
+        merged_arr[k] = arr1[i];
+        ++i;
+        ++k;
+    }
+
+    while(j < size2)
+    {
+        merged_arr[k] = arr2[j];
+        ++j;
+        ++k;
     }
 }
 
