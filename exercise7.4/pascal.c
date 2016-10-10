83c6a1b1d77383e22c4d6eabd33e292a23d67b35
mvasanth <mvasanth@mvasanth-VirtualBox>
2016-10-09 21:19:01 -0400

    Implemented exercise 7.4

---
+++ exercise7.4/pascal.c
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+#include "pascal.h"
+
+#include <stdio.h>
+
+void populate_pascal_triangle(int* p_mat, int size)
+{
+    int (*mat)[size] = (int (*)[size])p_mat; 
+    
+    int i, j;
+    
+    for(i = 0; i < size; ++i)
+    {
+        for(j = 0; j < size; ++j)
+        {
+            if(i == j || j == 0)
+            {
+                mat[i][j] = 1;
+            }
+            else
+            {
+                if(j > i)
+                // do nothing 
+                ;
+                else
+                    mat[i][j] = mat[i-1][j-1] + mat[i-1][j];
+            }
+        }
+    }
+}
+
+void print_matrix(int* matp, int size)
+{
+    int (*mat)[size] = (int (*)[size])matp; 
+    
+    for(int r = 0; r < size; ++r)
+    {
+        for(int c = 0; c < size; ++c)
+        {
+            if(c <= r)
+                printf("%d     ", mat[r][c]);
+        }
+        printf("\n");
+    }
+}
---
+++ exercise7.4/pascal.h
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   pascal.h
+ * Author: mvasanth
+ *
+ * Created on October 9, 2016, 8:45 PM
+ */
+
+#ifndef PASCAL_H
+#define PASCAL_H
+
+void populate_pascal_triangle(int* p_mat, int size);
+
+void print_matrix(int* p_mat, int size);
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PASCAL_H */
+
